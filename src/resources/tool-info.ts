a0bc92069aa19bc59553f3c5deb361d4d7b1e3c9
j0hanz <golfarenlj@gmail.com>
2026-05-07 08:00:46 +0200

    fix: properly slim tool-info.ts to 4 nav helpers only

--- src/resources/tool-info.ts
+++ src/resources/tool-info.ts
@@ -5,7 +5,7 @@ export function getToolContracts(): ToolContract[] {
   return ALL_TOOLS;
 }
 
-function getSortedToolContracts(): ToolContract[] {
+export function getSortedToolContracts(): ToolContract[] {
   return [...ALL_TOOLS].sort((left, right) =>
     left.name.localeCompare(right.name)
   );
